dec5e5f7727781f8a733453eb6fdd4c9dd88fa87
m.khrabrykh <neonexer12@gmail.com>
2026-09-17 22:19:20 +0300

    feat: добавлен CORS middleware для обработки запросов

--- internal/core/transport/http/middleware/common.go
+++ internal/core/transport/http/middleware/common.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/neonexer/konvert-backend/internal/core/logger"
+	core_logger "github.com/neonexer/konvert-backend/internal/core/logger"
 	core_http_response "github.com/neonexer/konvert-backend/internal/core/transport/http/response"
 	"go.uber.org/zap"
 )
@@ -14,6 +14,35 @@ import (
 const (
 	requestIDHeader = "X-Request-ID"
 )
+
+func CORS() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// allowedOrigins := map[string]struct{} {
+			// 	"http://localhost:5050": {},
+			// }
+
+			// origin := r.Header.Get("Origin")
+			// if _, ok := allowedOrigins[origin]; ok {
+			// 	w.Header().Set("Access-Control-Allow-Origin", origin)
+			// 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+			// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			// }
+
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
 
 func RequestID() Middleware {
 	return func(next http.Handler) http.Handler {
